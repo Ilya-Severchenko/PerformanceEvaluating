c7d70b2022a9e33abde90e212e85b3a3f4083376
Ilya_Severchenko <ilya.severchenko@gmail.com>
2020-06-18 16:53:56 +0300

    Add Dependency Injection; update installed packages

---
+++ UKAD_Test/PerformanceEvaluating/DependencyInjection/AutofacConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using PerformanceEvaluating.Business.Interfaces;
+using PerformanceEvaluating.Business.Repositories;
+using PerformanceEvaluating.Data;
+using System.Web.Mvc;
+
+namespace PerformanceEvaluating.DependencyInjection
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            builder.RegisterType<RequestResultRepository>().As<IRequestResultRepository>()
+                .WithParameter("context", new ApplicationDbContext());
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ UKAD_Test/PerformanceEvaluating/ViewModel/RequestResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerformanceEvaluating.ViewModel
+{
+    public class RequestResultViewModel
+    {
+        public string Url { get; set; }
+        public long Min { get; set; }
+        public long Max { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
